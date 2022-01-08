cc2734ec72edd53bc0b3c03c13ee88d2ba133f99
ArinKothari <96499331+ArinKothari@users.noreply.github.com>
2022-01-08 21:23:55 +0530

    Add files via upload

---
+++ asg1_CSE_21103047/asg1_CSE_21103047 OUTPUT.py
@@ -0,0 +1,34 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\College\Projects\pythonfiles\asg1_CSE_21103047.py ========
+Answer 1.
+Enter number 1. - 10
+Enter number 2. - 20
+Enter number 3. - 30
+20.0
+
+
+Answer 2.
+Enter Gross Income - 567543.56
+Enter Dependents - 6
+tax: 107908.71
+
+
+Answer 3.
+Enter SID:21103047
+Enter Name:Arin Kothari
+Enter Gender(F, M, or U):M
+Enter Course Name:CSE
+Enter CGPA:8.9
+['21103047', 'Arin Kothari', 'M', 'CSE', 8.9]
+
+
+Answer 4.
+Enter the marks of all the 5 students:56 65 76 43 89
+[43, 56, 65, 76, 89]
+
+
+Answer 5.
+color ['Red', 'Green', 'White', 'Pink', 'Yellow']
+color ['Red', 'Green', 'White', 'Purple', 'Yellow']
