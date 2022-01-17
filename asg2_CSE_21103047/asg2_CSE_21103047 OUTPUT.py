8afcb7d59e5c6df77e6499f74e1ceb7c8190b0fc
ArinKothari <96499331+ArinKothari@users.noreply.github.com>
2022-01-17 19:07:10 +0530

    Add files via upload

---
+++ asg2_CSE_21103047/asg2_CSE_21103047 OUTPUT.py
@@ -0,0 +1,40 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\College\Projects\pythonfiles\asg2_CSE_21103047.py ========
+Answer 1.
+a. 35
+b. egaugnal evitisnes esac a si nohtyP
+c. a case sensitive language
+d. Python is object oriented language
+e. 10
+f. Pythonisacasesensitivelanguage
+
+
+Answer 2.
+Hey, Arin Kothari Here!
+My SID is 21103047
+I am from CSE department and my CGPA is 9.9
+
+
+Answer 3.
+a. 8
+b. 58
+c. 50
+d. New values of a and b are  224 and 40
+e. New values of a and b are  14 and 2
+
+
+Answer 4.
+Input 3 values:67 8999 76
+Greatest value is  8999
+
+
+Answer 5.
+Enter a string:My name is Arin Kothari
+Yes
+
+
+Answer 6.
+Input the length of 3 sides:678 654 45
+Yes
