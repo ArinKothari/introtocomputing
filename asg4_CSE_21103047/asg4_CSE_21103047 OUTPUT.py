612e2deda03d816d7ca43bf8ace8cb0c50900e0b
ArinKothari <96499331+ArinKothari@users.noreply.github.com>
2022-02-26 15:07:57 +0530

    Add files via upload

---
+++ asg4_CSE_21103047/asg4_CSE_21103047 OUTPUT.py
@@ -0,0 +1,81 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\College\Projects\pythonfiles\asg4_CSE_21103047.py ========
+Answer 1.
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+Answer 2.
+Enter the number of rows in Pascal's Triangle: 11
+
+Using recursions:
+
+                    1   
+                  1   1   
+                1   2   1   
+              1   3   3   1   
+            1   4   6   4   1   
+          1   5   10   10   5   1   
+        1   6   15   20   15   6   1   
+      1   7   21   35   35   21   7   1   
+    1   8   28   56   70   56   28   8   1   
+  1   9   36   84   126   126   84   36   9   1   
+1   10   45   120   210   252   210   120   45   10   1   
+
+Using loops:
+
+                    1   
+                  1   1   
+                1   2   1   
+              1   3   3   1   
+            1   4   6   4   1   
+          1   5   10   10   5   1   
+        1   6   15   20   15   6   1   
+      1   7   21   35   35   21   7   1   
+    1   8   28   56   70   56   28   8   1   
+  1   9   36   84   126   126   84   36   9   1   
+1   10   45   120   210   252   210   120   45   10   1   
+
+
+Answer 3.
+Enter 2 numbers: 56 9
+The quotient is 6 and reminder is 2
+a. The quotient and reminder is a callable value.
+False
+False
+b. All the values are non zero
+c. Filtered out numbers: [2, 4]
+d. Set: {2, 4}
+e. Immutable set: frozenset({2, 4})
+f. Hash value of the max value from the above set: 4
+
+
+Answer 4.
+Object created
+The name of the student is Arin Kothari and SID is 21103047.
+Object destroyed
+
+
+Answer 5.
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+
+
+Answer 6.
+Enter the first word: arin
+
+Enter a new meaningful word to test your friendship: rain
+
+{For the Shopkeeper}
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
