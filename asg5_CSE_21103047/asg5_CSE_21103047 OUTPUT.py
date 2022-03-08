396166b6a4227da6ac1df992752e9fb675b4a970
ArinKothari <96499331+ArinKothari@users.noreply.github.com>
2022-03-08 22:48:42 +0530

    Add files via upload

---
+++ asg5_CSE_21103047/asg5_CSE_21103047 OUTPUT.py
@@ -0,0 +1,62 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\College\Projects\pythonfiles\asg5_CSE_21103047.py ========
+Answer 1.
+
+
+Answer 2.
+                                  2018
+
+      January                   February                   March
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7                1  2  3  4                1  2  3  4
+ 8  9 10 11 12 13 14       5  6  7  8  9 10 11       5  6  7  8  9 10 11
+15 16 17 18 19 20 21      12 13 14 15 16 17 18      12 13 14 15 16 17 18
+22 23 24 25 26 27 28      19 20 21 22 23 24 25      19 20 21 22 23 24 25
+29 30 31                  26 27 28                  26 27 28 29 30 31
+
+       April                      May                       June
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+                   1          1  2  3  4  5  6                   1  2  3
+ 2  3  4  5  6  7  8       7  8  9 10 11 12 13       4  5  6  7  8  9 10
+ 9 10 11 12 13 14 15      14 15 16 17 18 19 20      11 12 13 14 15 16 17
+16 17 18 19 20 21 22      21 22 23 24 25 26 27      18 19 20 21 22 23 24
+23 24 25 26 27 28 29      28 29 30 31               25 26 27 28 29 30
+30
+
+        July                     August                  September
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+                   1             1  2  3  4  5                      1  2
+ 2  3  4  5  6  7  8       6  7  8  9 10 11 12       3  4  5  6  7  8  9
+ 9 10 11 12 13 14 15      13 14 15 16 17 18 19      10 11 12 13 14 15 16
+16 17 18 19 20 21 22      20 21 22 23 24 25 26      17 18 19 20 21 22 23
+23 24 25 26 27 28 29      27 28 29 30 31            24 25 26 27 28 29 30
+30 31
+
+      October                   November                  December
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7                1  2  3  4                      1  2
+ 8  9 10 11 12 13 14       5  6  7  8  9 10 11       3  4  5  6  7  8  9
+15 16 17 18 19 20 21      12 13 14 15 16 17 18      10 11 12 13 14 15 16
+22 23 24 25 26 27 28      19 20 21 22 23 24 25      17 18 19 20 21 22 23
+29 30 31                  26 27 28 29 30            24 25 26 27 28 29 30
+                                                    31
+
+
+
+Answer 3.
+298224
+
+
+Answer 4.
+Enter the array: 56 778 655 54 3 6 99999
+Sorted array: [3, 6, 54, 56, 655, 778, 99999]
+
+
+Answer 5.
+Enter an integer array  containing duplicates: 5 5 5 44 3 3 3 3 5 5 7 7 
+a. Sorted array:  [3, 3, 3, 3, 5, 5, 5, 5, 5, 7, 7, 44]
+Enter the number to find: 3
+b. Element is present at index 2
+c. The number of occurrences of 3 is 4
